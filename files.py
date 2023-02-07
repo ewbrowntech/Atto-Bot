b36ef3beba5bc37555fb1a7cab6017027127fe5c
Ethan Brown <ewbrowntech@gmail.com>
2023-02-07 11:59:05 -0600

    Enabled the posting of file AttoHost address, and switched atto-address config to JSON format

--- files.py
+++ files.py
@@ -1,26 +1,28 @@
 import os
 import discord
-from integrations.Atto.Atto import upload_file
+from integrations.Atto.Atto import get_atto_addresses, upload_file
 '''
 files.py
 
 @Author - Ethan Brown - ewbrowntech@gmail.com
-@Version - 29 JAN 23
+@Version - 07 FEB 23
 
 Manage the storage and serving of files used by Atto-Bot
 '''
 
 async def serve_file(status, filepath):
     server = status.channel.guild
-    uploadLimit = server.filesize_limit# The maximum file upload size in bytes
+    uploadLimit = server.filesize_limit  # The maximum file upload size in bytes
     filesize = os.stat(filepath).st_size
     if not filesize > uploadLimit:
         # await status.edit("Here you go!", file=discord.File(filepath))
         url = upload_file(filepath)
-        await status.edit("File uploaded to AttoHost: " + url)
+        await status.edit("This file is too big for Discord, so I uploaded it to AttoHost!\n"
+                          + get_atto_addresses()["download"] + url)
     else:
         url = upload_file(filepath)
-        await status.edit("File uploaded to AttoHost: " + url)
+        await status.edit("This file is too big for Discord, so I uploaded it to AttoHost!\n"
+                          + get_atto_addresses()["download"] + url)
 
 def delete_file(filepath):
     os.remove(filepath)
--- integrations/Atto/Atto.py
+++ integrations/Atto/Atto.py
@@ -1,15 +1,18 @@
 import AttoAPI
-
+import json
 
 def get_apikey():
     with open('config/tokens/atto-api-key', 'r') as file:
         return file.read()
 
-def get_atto_address():
-    with open('config/atto-address', 'r') as file:
-        return file.read()
+def get_atto_addresses():
+    address_file = open('config/atto-addresses.json', 'r')
+    addresses = json.load(address_file)
+    address_file.close()
+    return addresses
+
 def upload_file(filepath):
-    address = get_atto_address()
+    address = get_atto_addresses()["upload"]
     apikey = get_apikey()
     url = AttoAPI.upload_file(filepath, address, apikey)
     return url
